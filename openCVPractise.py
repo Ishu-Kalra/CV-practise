1773c93882bd151516f340719f30b781e3aeffde
ki <ishukalraish2@gmail.com>
2018-06-05 15:42:18 +0530

    All p;ractise files will be continously change reflecting the checkpoints where I am at :D

--- openCVPractise.py
+++ openCVPractise.py
@@ -1,11 +1,14 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('assets/images/puppy-boundaries_header.jpg', 0)
-#cv2.imshow('image', img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+img = cv2.imread('assets/images/puppyGray.png', 0)
+
 cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+k = cv2.waitKey(0) & 0xFF
+
+if k == 27:
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('puppyCutieGray.png', img)
+    cv2.destroyAllWindows()
+
